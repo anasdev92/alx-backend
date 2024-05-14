7177566ae86b1b55e47af47c2f3f0512c72b8f79
anasdev92 <elyamni.anas@gmail.com>
2024-05-14 11:01:08 -0700

    second commit

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,21 +1,16 @@
-
 #!/usr/bin/env python3
-"""Module for task 0
-"""
+'''Task 0: Basic Flask app
+'''
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route("/")
-def index() -> str:
-    """The index function displays the home page of the web application.
-
-    Returns:
-        str: contents of the home page.
-    """
-    return render_template("0-index.html")
+@app.route('/')
+def index():
+    '''default route'''
+    return render_template("0-index.html",)
 
 
 if __name__ == "__main__":
